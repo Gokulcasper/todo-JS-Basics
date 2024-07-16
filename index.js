6a0335c8e6ce9add0b43780a447866bcc1378ef4
Gokulcasper <ronitcasper215@gmail.com>
2024-07-16 21:33:27 +0530

    commit Delete icon functionality

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 let addItemContainer = document.getElementById("todoList-id");
+let todoInputId = document.getElementById("todo-id-input");
+let todoAddButton = document.getElementById("todo-id-addButton");
 
 let todoList= [
     {
         text:" Learn Html ",
+        id:1
     },
     {
         text:" Learn Css ",
+        id:2
     },
     {
         text:" Learn JS ",
+        id:3
     },
 ]
-function createAppendTodo(todo){
+
+
+
+let todoListCounts = todoList.length;
+console.log(todoListCounts);
+function createAndAppendTodo(todo){
+    let todoId ="todo" + todo.id;
+    let checkboxId ="checkbox" + todo.id;
+    let label ="label" + todo.id;
+
+
     let liElement = document.createElement("li");
 liElement.classList.add("todo-li-class","d-flex","flex-row");
 addItemContainer.appendChild(liElement);
@@ -19,7 +34,7 @@ addItemContainer.appendChild(liElement);
 let inputElement = document.createElement("input");
 inputElement.type="checkbox";
 inputElement.classList.add("checkboxClassInput");
-inputElement.id="addCheckbox";
+inputElement.id=checkboxId;
 liElement.appendChild(inputElement);
 
 let divElement = document.createElement("div");
@@ -28,7 +43,7 @@ liElement.appendChild(divElement);
 
 let labelElement = document.createElement("label");
 labelElement.textContent=todo.text;
-labelElement.setAttribute("for","addCheckbox");
+labelElement.setAttribute("for",checkboxId);
 labelElement.classList.add("labelClassInput");
 divElement.appendChild(labelElement);
 
@@ -39,26 +54,37 @@ divElement.appendChild(deleteDivElement);
 let deleteIcon = document.createElement("i");
 deleteIcon.classList.add("ri-delete-bin-fill", "delete-icon" );
 deleteDivElement.appendChild(deleteIcon)
+
+deleteIcon.onclick=function(){
+    onDeleteTodo(todoId);
+};
+}
+
+for(let todo of todoList){
+createAndAppendTodo(todo);
+}
+
+
+function onAddTodo(){
+   let userInputValue = todoInputId.value;
+   console.log(userInputValue);
+   if(userInputValue == ""){
+    alert("Enter Value");
+    return;
+   }
+   todoListCounts = todoListCounts+1;
+   let newTodo = {
+    text:userInputValue,
+    id:todoListCounts,
+   };
+
+   createAndAppendTodo(newTodo);
+   todoInputId.value= " ";
 }
 
 
 
 
-
-// let inputElement = document.createElement("input");
-// inputElement.type="checkbox";
-// inputElement.id="addCheckbox";
-// inputElement.classList.add("checkboxClassInput")
-// addItemContainer.appendChild(inputElement);
-
-// let labelElement = document.createElement("label");
-// labelElement.textContent="Gokul";
-// addItemContainer.appendChild(labelElement);
-// labelElement.setAttribute("for","addCheckbox")
-// labelElement.classList.add("labelClassInput");
-
-// let myArray=[1,2,3,4,5];
-// console.log(myArray);
-for(let todo of todoList){
-createAppendTodo(todo);
+todoAddButton.onclick=function(){
+    onAddTodo();
 }
